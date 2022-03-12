c380d772fb940f7812b4c479acda94b258b2b3d7
Soon Keat NEO <neo@soonke.at>
2022-03-13 02:43:23 +0800

    Create listener.py

---
+++ listener.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import socket
+import sys
+
+if (len(sys.argv) != 2 or not sys.argv[1].isdigit()):
+  print('Usage: listen <port>')
+  exit()
+
+p = int(sys.argv[1])
+l = []
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', p))
+s.listen(1)
+while 1:
+  (c, a) = s.accept()
+  l.append(c)
